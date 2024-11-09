6622882d599bee48d4266f9b7fee305f802f687f
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-11-09 13:27:17 +0530

    add logout and checkAuth functionality; update auth slice with new actions and enhance Profile component layout

--- client/src/redux/slice/auth-slice.js
+++ client/src/redux/slice/auth-slice.js
@@ -38,11 +38,30 @@ export const login = createAsyncThunk("auth/login", async (data, thunkApi) => {
   }
 });
 
+export const logout = createAsyncThunk("auth/logout", async (_, thunkApi) => {
+  try {
+    const response = await axios.get("http://localhost:5000/api/auth/logout");
+    return response.data;
+  } catch (error) {
+    return thunkApi.rejectWithValue(error.response.data);
+  }
+})
+
+export const checkAuth = createAsyncThunk("auth/checkAuth", async (_, thunkApi) => {
+  try {
+    const response = await axios.get("http://localhost:5000/api/auth/check-auth");
+    return response.data;
+  } catch (error) {
+    return thunkApi.rejectWithValue(error.response.data);
+  }
+})
+
 const authSlice = createSlice({
   name: "auth",
   initialState: {
     data: null,
     isLoading: false,
+    isAuthenticated: false,
     isError: false
   },
   extraReducers: (builder) => {
@@ -50,6 +69,7 @@ const authSlice = createSlice({
         state.data = action.payload;
         state.isLoading = false
         state.isError = false
+        state.isAuthenticated = true
     })
 
     builder.addCase(register.pending, (state, action) => {
@@ -66,6 +86,7 @@ const authSlice = createSlice({
         state.data = action.payload;
         state.isLoading = false
         state.isError = false
+        state.isAuthenticated = true
     })
 
     builder.addCase(login.pending, (state, action) => {
@@ -76,6 +97,40 @@ const authSlice = createSlice({
     builder.addCase(login.rejected, (state, action) => {
         state.isLoading = false
         state.isError = true
+    })
+
+    builder.addCase(logout.fulfilled, (state, action) => {
+      state.isAuthenticated = false
+      state.data = null
+      state.isLoading = false
+      state.isError = false
+    })
+
+    builder.addCase(logout.pending, (state, action) => {
+      state.isLoading = true
+      state.isError = false
+    })
+
+    builder.addCase(logout.rejected, (state, action) => {
+      state.isLoading = false
+      state.isError = true
+    })
+
+    builder.addCase(checkAuth.fulfilled, (state, action) => {
+      state.isAuthenticated = true
+      state.data = action.payload
+      state.isLoading = false
+      state.isError = false
+    })
+
+    builder.addCase(checkAuth.pending, (state, action) => {
+      state.isLoading = true
+      state.isError = false
+    })
+
+    builder.addCase(checkAuth.rejected, (state, action) => {
+      state.isLoading = false
+      state.isError = true
     })
   }
 });
--- client/src/screen/Profile.js
+++ client/src/screen/Profile.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import img1 from '../assets/dl.beatsnoop 1.png'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div className='px-28'>
+      {/*  */}
+      <div className='flex justify-between mt-20 mb-20'>
+        <h1>Home / MyAccount</h1>
+        <div>
+          <h1>Welcome! <span className='text-[#e07575]'>Sandeep Singh</span></h1>
+        </div>
+      </div>
+
+      {/* Profile */}
+      <div className='flex justify-between items-center'>
+        <div className='w-2/3 flex items-center'>
+          <img src={img1} alt='not showing' className='rounded-full w-56 h-56 object-cover'/>
+        </div>
+        <div className=''>
+          <h1 className='text-3xl'>Profile</h1>
+          <div className='mt-10'>
+            <h1 className='text-xl'>Name: Sandeep Singh</h1>
+            <h1 className='text-xl'>Name: Sandeep Singh</h1>
+            <h1 className='text-xl'>Name: Sandeep Singh</h1>
+            <h1 className='text-xl'>Name: Sandeep Singh</h1>
+            <h1 className='text-xl'>Name: Sandeep Singh</h1>
+            <h1 className='text-xl'>Name: Sandeep Singh</h1>
+          </div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
