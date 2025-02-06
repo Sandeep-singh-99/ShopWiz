96d76359c50a6d741a169136cc49650b3c9601a9
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-02-06 16:43:45 +0530

    refactor: update CORS configuration for local development and specify Node.js engine version

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ if (cluster.isMaster) {
   app.use(express.urlencoded({ extended: true }));
 
   app.use(cors({
-    origin:"https://shop-wiz.vercel.app",
+    origin: "http://localhost:3000", //"https://shop-wiz.vercel.app",
     credentials: true,
   }))
   
